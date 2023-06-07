715e1c8af6311d98427d0fe34bd495b5affda7f2
Leticia <leticiaagsmp@gmail.com>
2023-06-07 05:28:11 -0300

    refatorado o main

---
+++ teste/src/Exemplo.java
@@ -0,0 +1,9 @@
+public class Exemplo {
+    private static void sayHello() {
+        System.out.println("Olá Mundo!");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
