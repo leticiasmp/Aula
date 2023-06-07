772a10f61801cd1ed4bcc4b6512444f853bc2a38
Leticia <leticiaagsmp@gmail.com>
2023-06-07 05:30:18 -0300

    adicionado hellomanytimes

--- teste/src/Exemplo.java
+++ teste/src/Exemplo.java
@@ -1,9 +1,16 @@
 public class Exemplo {
+
+    private static void sayHelloManyTimes(int times) {
+        for (int i = 0; i < times; i++) {
+            sayHello();
+        }
+    }
+
     private static void sayHello() {
         System.out.println("Olá Mundo!");
     }
 
     public static void main(String[] args) {
-        sayHello();
+        sayHelloManyTimes();
     }
 }
